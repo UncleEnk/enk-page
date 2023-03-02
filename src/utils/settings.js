92120b9c07ef9acae36ac505335822768589de3b
UncleEnk <81113282+UncleEnk@users.noreply.github.com>
2023-03-01 19:25:21 -0800

    config + font change now require aesthetic iosevka

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,10 +1,10 @@
 const defaultConfig = {
-	username: "Excalith",
+	username: "uncleenk",
 	terminal: {
 		fixedHeight: true,
 		backgroundColor: "#121317",
 		windowColor: "#1e212b",
-		glowColor: "#6b5cb157"
+		glowColor: "#"
 	},
 	prompt: {
 		ctrlC: true,
@@ -37,11 +37,11 @@ const defaultConfig = {
 		titleColor: "yellow"
 	},
 	urlLaunch: {
-		target: "_self"
+		target: "_blank"
 	},
 	search: {
 		default: "https://google.com/search?q=",
-		target: "_self",
+		target: "_blank",
 		shortcutRegex: "([A-Za-z0-9]+) (.*)",
 		shortcuts: [
 			{
@@ -50,29 +50,14 @@ const defaultConfig = {
 				url: "https://google.com/search?q={}"
 			},
 			{
-				alias: "d",
-				name: "DuckDuckGo Search",
-				url: "https://duckduckgo.com/?q={}"
-			},
-			{
-				alias: "b",
-				name: "Brave Search",
-				url: "https://search.brave.com/search?q={}"
-			},
-			{
 				alias: "gh",
 				name: "Github Search",
 				url: "https://github.com/search?q={}"
 			},
 			{
-				alias: "s",
-				name: "Stack Overflow Search",
-				url: "https://stackoverflow.com/search?q={}"
-			},
-			{
 				alias: "r",
 				name: "Subreddit Search",
-				url: "https://reddit.com/r/{}"
+				url: "https://r.nf/r/{}"
 			},
 			{
 				alias: "w",
@@ -87,24 +72,14 @@ const defaultConfig = {
 			color: "green",
 			links: [
 				{
-					name: "Portfolio",
-					url: "https://cancellek.com",
-					icon: "mdi:web"
-				},
-				{
-					name: "Keybase",
-					url: "https://keybase.io/",
-					icon: "fa-brands:keybase"
-				},
-				{
 					name: "GPT",
 					url: "https://chat.openai.com/",
 					icon: "simple-icons:openai"
 				},
 				{
-					name: "OCI",
-					url: "https://www.oracle.com/cloud/",
-					icon: "simple-icons:oracle"
+					name: "Invoke Ai",
+					url: "https://localhost:9090",
+					icon: "simple-icons:conventional-commits"
 				}
 			]
 		},
@@ -121,42 +96,6 @@ const defaultConfig = {
 					name: "GitLab",
 					url: "https://gitlab.com",
 					icon: "ph:gitlab-logo-simple-fill"
-				},
-				{
-					name: "Dev.to",
-					url: "https://dev.to",
-					icon: "material-symbols:logo-dev"
-				},
-				{
-					name: "Stack Overflow",
-					url: "https://stackoverflow.com/",
-					icon: "mdi:stack-overflow"
-				}
-			]
-		},
-		{
-			title: "Social",
-			color: "purple",
-			links: [
-				{
-					name: "Twitter",
-					url: "https://twitter.com",
-					icon: "mdi:twitter"
-				},
-				{
-					name: "Mastodon",
-					url: "https://mastodon.social/",
-					icon: "ri:mastodon-fill"
-				},
-				{
-					name: "Reddit",
-					url: "https://reddit.com",
-					icon: "mdi:reddit"
-				},
-				{
-					name: "Polywork",
-					url: "https://polywork.com",
-					icon: "simple-icons:polywork"
 				}
 			]
 		},
@@ -165,9 +104,9 @@ const defaultConfig = {
 			color: "cyan",
 			links: [
 				{
-					name: "Polygon",
-					url: "https://polygon.com",
-					icon: "uil:polygon"
+					name: "Hitmaps",
+					url: "https://hitmaps.com",
+					icon: "simple-icons:Hetzner"
 				},
 				{
 					name: "IGN",
@@ -175,14 +114,9 @@ const defaultConfig = {
 					icon: "mdi:currency-sign"
 				},
 				{
-					name: "RPS",
-					url: "https://rockpapershotgun.com/",
-					icon: "ph:toilet-paper-bold"
-				},
-				{
-					name: "80lv",
-					url: "https://80.lv/",
-					icon: "tabler:hand-rock"
+					name: "Fandom",
+					url: "https://fandom.com/",
+					icon: "simple-icons:fandom"
 				}
 			]
 		},
@@ -190,16 +124,6 @@ const defaultConfig = {
 			title: "Science",
 			color: "blue",
 			links: [
-				{
-					name: "PopSci",
-					url: "https://popsci.com/",
-					icon: "material-symbols:science"
-				},
-				{
-					name: "Space",
-					url: "fa6-solid:user-astronaut",
-					icon: "mdi:reddit"
-				},
 				{
 					name: "NASA",
 					url: "https://blogs.nasa.gov/",
@@ -209,32 +133,6 @@ const defaultConfig = {
 					name: "ESA",
 					url: "https://blogs.esa.int/",
 					icon: "mdi:black-mesa"
-				}
-			]
-		},
-		{
-			title: "Tech",
-			color: "yellow",
-			links: [
-				{
-					name: "TechCrunch",
-					url: "https://techcrunch.com/",
-					icon: "game-icons:techno-heart"
-				},
-				{
-					name: "Verge",
-					url: "https://www.theverge.com/",
-					icon: "arcticons:verge"
-				},
-				{
-					name: "It's Foss",
-					url: "https://itsfoss.com/",
-					icon: "ri:mastodon-fill"
-				},
-				{
-					name: "9To5 Linux",
-					url: "https://9to5linux.com/",
-					icon: "uil:linux"
 				}
 			]
 		}
